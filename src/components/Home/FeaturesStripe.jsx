84e77850309efa96a4f9b05a124089200346bd68
shashank chourasia01 <shashankchourasia0829@gmail.com>
2026-04-21 01:28:25 +0530

    qq

--- src/components/Home/FeaturesStripe.jsx
+++ src/components/Home/FeaturesStripe.jsx
@@ -10,7 +10,7 @@ const FeaturesStripe = ({ style = {} }) => {
 
   return (
     <div style={{ backgroundColor: '#e8f4f8', borderBottom: '1px solid #f3f4f6',marginTop: '12px', ...style }}>
-      <div style={{ padding: '12px 16px' }}>
+      <div style={{ padding: '8px 16px' }}>
         <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-around', gap: '8px' }}>
           {features.map((feature, index) => {
             const Icon = feature.icon;
