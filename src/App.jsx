996822451d9ef93d624fe28f692b01b863bf3314
shashank chourasia01 <shashankchourasia0829@gmail.com>
2026-04-27 21:01:05 +0530

    qf

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,14 @@ import Header from './components/Layout/Header';
 import AnnouncementTape from './components/Layout/AnnouncementTape';
 import HomePage from './pages/HomePage';
 
+import ComingSoon from './components/ComingSoon';
+
 function App() {
   return (
     <div className="min-h-screen bg-white">
+
+      <ComingSoon />
+
       {/* <AnnouncementTape />
       <Header />
       <HomePage /> */}
